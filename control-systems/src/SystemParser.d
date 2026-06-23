8461089dd36a24c31988f98d223f22a8f632931c
Andel Vitório <andel.js@outlook.com>
2026-06-23 13:31:59 -0400

    chore: file organization

--- control-systems/src/SystemParser.d
+++
@@ -1,5 +0,0 @@
-src/SystemParser.o: src/SystemParser.cpp include/SystemParser.hpp \
- include/SystemModel.hpp include/../lib/json.hpp
-include/SystemParser.hpp:
-include/SystemModel.hpp:
-include/../lib/json.hpp:
